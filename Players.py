26408144238de924c5f109049b9ca4dc4c3734df
Frixos M <98382221+Frixosmm@users.noreply.github.com>
2022-01-25 14:34:09 +0100

    1.1?

--- Players.py
+++ Players.py
@@ -11,15 +11,17 @@ class Player(object):
     bid = 0
     turn=0
     cards=[(0,0),(0,0)]
-    # The class "constructor" - It's actually an initializer 
-    def __init__(self, name, money, bid, turn,cards):
+    value=0
+    # The class "constructor" - It's actually an initializer
+    def __init__(self, name, money, bid, turn,cards,value):
         self.name = name
         self.money = money
         self.bid = bid
         self.turn=turn
         self.cards=cards
+        self.value=value
 def make_player(name, money, bid,turn,cards):
-    player = Player(name, money, bid,turn,cards)
+    player = Player(name, money, bid,turn,cards,0)
     return player
 
 
