2382ae3710c02b15f44ad8d3972b2d4ed916597d
Frixos M <98382221+Frixosmm@users.noreply.github.com>
2022-06-21 23:54:31 +0200

    notes

--- Decide_move.py
+++ Decide_move.py
@@ -2,3 +2,11 @@
 
 #Will alter Player.move according to decision
 #Can start smaller. Input is p.value,p.money,p.bid,pot,
+
+
+def Decide_move(player):
+    move="fold"
+    if player.value>=2000:
+        move="call"
+
+    return move
